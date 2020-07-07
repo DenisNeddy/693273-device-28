a95e123879e088a999f84dc0efce004270315ae9
Denis <fordz2012@gmail.com>
2020-07-07 18:18:47 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var modal = document.querySelector('.modal');
+var buttonModalClose = document.querySelector('.modal-close-button');
+var buttonModalOpen = document.querySelector('.contacts-button');
+buttonModalClose.classList.add('red');
+
