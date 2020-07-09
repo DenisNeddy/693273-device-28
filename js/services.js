1dd44f741aea8133b7ae85683496af36be5984ea
Denis <fordz2012@gmail.com>
2020-07-09 17:35:42 +0300

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,23 +1,6 @@
-var paginationButtons = document.querySelectorAll(".pagination-button");
-var slider = document.querySelectorAll(".slider-item")
+var buttonWarranty = document.querySelector(".button-warranty");
 
-
-
-paginationButton[0].addEventListener("click", function(evt) {
-  for(let slader of sliders) {
+buttonWarranty.addEventListener("click", function(evt) {
   evt.preventDefault();
-  slider.classList.toggle("open-slider");
-  }
-});
-
-paginationButton[1].addEventListener("click", function(evt) {
-  evt.preventDefault();
-  slider[0].classList.remove("open-slider");
-  slider[1].classList.add("open-slider");
-});
-
-paginationButton[2].addEventListener("click", function(evt) {
-  evt.preventDefault();
-  slider[1].classList.remove("open-slider");
-  slider[2].classList.add("open-slider");
-});
+  buttonWarranty.classList.add("services-button-active");
+})
