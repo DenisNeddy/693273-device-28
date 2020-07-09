e3a6489a425c86d65e10e5a11e8d29df0e653078
Denis <fordz2012@gmail.com>
2020-07-09 13:36:25 +0300

    Create modal-map.js

---
+++ js/modal-map.js
@@ -0,0 +1,13 @@
+var map = document.querySelector(".map-popup");
+var CloseMapButton = document.querySelector(".close-map-button");
+var OpenMapButton = document.querySelector(".contacts-map-button");
+
+OpenMapButton.addEventListener("click", function(evt) {
+ evt.preventDefault();
+ map.classList.add("open-map-popup");
+});
+
+CloseMapButton.addEventListener("click", function(evt) {
+ evt.preventDefault();
+ map.classList.remove("open-map-popup");
+});
