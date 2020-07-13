ca8bd8a0e6a3230944e7c5574e7576f990837478
Denis <fordz2012@gmail.com>
2020-07-13 21:15:36 +0300

    Delete services.js

--- js/services.js
+++
@@ -1,6 +0,0 @@
-var buttonWarranty = document.querySelector(".button-warranty");
-
-buttonWarranty.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  buttonWarranty.classList.add("services-button-active");
-})
